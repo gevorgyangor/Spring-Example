6b26a197311fc559aa4758672da7473a8d457f5b
Gor <gevorgyan604@gmail.com>
2018-03-25 21:43:53 +0400

    add AdvanceBlog project

---
+++ AdvancedBlog/src/main/java/com/example/advancedblog/AdvancedBlogApplication.java
@@ -0,0 +1,12 @@
+package com.example.advancedblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedBlogApplication.class, args);
+	}
+}
---
+++ AdvancedBlog/src/test/java/com/example/advancedblog/AdvancedBlogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.advancedblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdvancedBlogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
