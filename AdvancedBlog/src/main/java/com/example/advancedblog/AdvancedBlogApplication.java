4b3f2d5a97ec56caef099c25d73955aa5e80a7cc
Gor <gevorgyan604@gmail.com>
2018-03-26 02:20:51 +0400

    add

--- AdvancedBlog/src/main/java/com/example/advancedblog/AdvancedBlogApplication.java
+++ AdvancedBlog/src/main/java/com/example/advancedblog/AdvancedBlogApplication.java
@@ -2,11 +2,19 @@ package com.example.advancedblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class AdvancedBlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdvancedBlogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedBlogApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
